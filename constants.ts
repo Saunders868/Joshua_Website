a12939fb916de74dd6e3ac90131aa807bbaf65c0
Daniel Saunders <danielsaunders@Daniels-MacBook-Pro.local>
2024-02-26 18:43:58 -0400

    changed fontend url constant

--- constants.ts
+++ constants.ts
@@ -5,8 +5,12 @@ export const PRODUCTS_URL: string = process.env.NEXT_PUBLIC_PRODUCTS_ENDPOINT!;
 export const CARTS_URL: string = process.env.NEXT_PUBLIC_CARTS_ENDPOINT!;
 export const ORDERS_URL: string = process.env.NEXT_PUBLIC_ORDERS_ENDPOINT!;
 export const MAIL_URL: string = process.env.NEXT_PUBLIC_MAIL_ENDPOINT!;
-export const FRONTEND_URL: string = "http://localhost:3000";
-export const PAYPAL_CREATE: string = process.env.NEXT_PUBLIC_PAYPAL_ORDER_CREATE!;
-export const PAYPAL_CAPTURE: string = process.env.NEXT_PUBLIC_PAYPAL_ORDER_CAPTURE!;
-export const PAYPAL_CLIENT_ID: string = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID!;
-export const PAYPAL_CLIENT_SECRET: string = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_SECRET!;
+export const FRONTEND_URL: string = process.env.NEXT_PUBLIC_FRONTEND_URL!;
+export const PAYPAL_CREATE: string =
+  process.env.NEXT_PUBLIC_PAYPAL_ORDER_CREATE!;
+export const PAYPAL_CAPTURE: string =
+  process.env.NEXT_PUBLIC_PAYPAL_ORDER_CAPTURE!;
+export const PAYPAL_CLIENT_ID: string =
+  process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID!;
+export const PAYPAL_CLIENT_SECRET: string =
+  process.env.NEXT_PUBLIC_PAYPAL_CLIENT_SECRET!;
