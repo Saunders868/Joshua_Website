a457f29c0371719dbd79578691084473603ca054
Daniel Saunders <saundersdaniel.10@gmail.com>
2024-10-31 00:55:25 -0400

    feature: added structured data to home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,77 @@ export const metadata: Metadata = {
   },
 };
 
+const googleJsonLd = {
+  "@context": "http://www.schema.org",
+  "@type": "Person",
+  name: "Joshua Greene",
+  alternateName: "joshuagreenee",
+  nationality: "Trinidadian",
+  birthPlace: {
+    "@type": "Place",
+    address: {
+      "@type": "PostalAddress",
+      addressLocality: "Princes Town",
+      addressRegion: "Caribbean",
+      addressCountry: "Trinidad",
+    },
+  },
+  gender: "Male",
+  description: "Trinidadian Food at Its Best: Joshua Greene's Expert Creations",
+  disambiguatingDescription:
+    "Trinidadian Food at Its Best: Joshua Greene's Expert Creations",
+  jobTitle: "Chef",
+  url: "https://joshua-website.vercel.app/",
+  image:
+    "https://joshua-website.vercel.app/opengraph-image.png?0e7a9af85d769048",
+  address: {
+    "@type": "PostalAddress",
+    addressLocality: "Princes Town",
+    addressRegion: "Caribbean",
+    addressCountry: "Trinidad",
+  },
+  sameAs: ["https://www.instagram.com/joshuagreenee/"],
+};
+
+const schemaORGJsonLd = {
+  "@context": "https://schema.org",
+  "@type": "ProfilePage",
+  dateCreated: "2024-10-23T12:34:00-05:00",
+  dateModified: "2024-10-26T14:53:00-05:00",
+  mainEntity: {
+    "@type": "Person",
+    name: "Joshua Greene",
+    description:
+      "Trinidadian Food at Its Best: Joshua Greene's Expert Creations",
+    alternateName: "joshuagreenee",
+    interactionStatistic: [
+      {
+        "@type": "InteractionCounter",
+        interactionType: "https://schema.org/FollowAction",
+        userInteractionCount: 1,
+      },
+      {
+        "@type": "InteractionCounter",
+        interactionType: "https://schema.org/LikeAction",
+        userInteractionCount: 5,
+      },
+      {
+        "@type": "InteractionCounter",
+        interactionType: "https://schema.org/BefriendAction",
+        userInteractionCount: 5,
+      },
+    ],
+    agentInteractionStatistic: {
+      "@type": "InteractionCounter",
+      interactionType: "https://schema.org/WriteAction",
+      userInteractionCount: 4,
+    },
+    image:
+      "https://joshua-website.vercel.app/opengraph-image.png?0e7a9af85d769048",
+    sameAs: ["https://www.instagram.com/joshuagreenee/"],
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -30,6 +101,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(googleJsonLd) }}
+          id="google-jsonld"
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaORGJsonLd) }}
+          id="schemaORG-jsonld"
+        />
+      </head>
       <body className={hankenGrotesk.className}>
         <AuthContext>
           <Providers>
